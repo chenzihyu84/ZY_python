6bdb41a695a12a1ffecc454d7ba36e10206c08f2
chenzihyu84 <150919672+chenzihyu84@users.noreply.github.com>
2023-12-10 04:13:11 +0000

    modify

---
+++ lesson8_2.py
@@ -0,0 +1,15 @@
+import math
+import pyinputplus as pyip
+
+def circle_area(radius):
+    area = radius ** 2 * math.pi
+    return area
+
+
+
+
+
+radius = pyip.inputFloat("請輸入半徑:")
+print(radius)
+area = circle_area(radius)
+print(f"半徑{radius},園面積是{area}")
