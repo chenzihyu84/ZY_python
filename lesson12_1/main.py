6123492b7e0d83d792f52a214334885ce606e3c9
chenzihyu84 <150919672+chenzihyu84@users.noreply.github.com>
2023-12-23 02:58:57 +0000

    modify

--- lesson12_1/main.py
+++ lesson12_1/main.py
@@ -1,36 +1,7 @@
-from random import randint,choices
-import csv
-from csv import DictWriter
+
 import pyinputplus as pyip
+from tools import getStudents,save_to_csv
 
-def getStudents(nums:int) -> list[dict]:
-    
- 
-    students:list[dict] = []
-    with open('names.txt', mode='r',encoding = 'utf-8') as file:
-        names:str = file.read()
-    namesList:list[str] = names.split('\n')
-    names = choices(namesList,k=nums)
-    
-    for i in range(nums):
-        stu = {
-        '姓名':names[i],
-        '國文':randint(45,100),
-        '數學':randint(45,100),
-        '英文':randint(45,100),
-        '地理':randint(45,100),
-        }
-        students.append(stu)
-    return students
-
-def save_to_csv(students:list[dict],fileName:str)->None:
-    fileNameWithExtension:list = fileName + '.csv'
-    with open(fileNameWithExtension,mode = 'w',encoding = 'utf-8',newline = '') as file:
-        fieldnames:list[str] = ['姓名','國文','數學','英文','地理']
-        writer:DictWriter= csv.DictWriter(file,fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(students)
-    print("寫入成功")
 
 if __name__ == "__main__":
     student_num:int = pyip.inputInt("請輸入學生人數(1~50):",min=1,max=50)
--- lesson12_1/tools.py
+++ lesson12_1/tools.py
@@ -1,38 +1,32 @@
-import random,csv
+from random import randint,choices
+import csv
+from csv import DictWriter
 
-def getStudents(student_num:int=1,scores_nums:int=2) -> list[list]:
-    '''
-    參數:student_num -> 學生人數
-    參數:scores_nums -> 科目數
-    '''
+def getStudents(nums:int) -> list[dict]:
+    
+ 
+    students:list[dict] = []
     with open('names.txt', mode='r',encoding = 'utf-8') as file:
         names:str = file.read()
-
     namesList:list[str] = names.split('\n')
-    students:list[list] = []
-
-    names:list[str] = random.choices(namesList,k=student_num)
-    for name in names:
-        stu:list[int|str] = []
-        stu.append(name)
-        for i in range(scores_nums):
-            stu.append(random.randint(40,100))
+    names = choices(namesList,k=nums)
+    
+    for i in range(nums):
+        stu = {
+        '姓名':names[i],
+        '國文':randint(45,100),
+        '數學':randint(45,100),
+        '英文':randint(45,100),
+        '地理':randint(45,100),
+        }
         students.append(stu)
-
     return students
 
-def savetoCSV(fileName:str,data:list[list],subject_nums:int) -> bool:
-    fileName += ".csv"
-    subjects = [f'科目{i+1}'for i in range(subject_nums)]
-    fields = ['姓名']
-    fields.extend(subjects)
-    with open(fileName,mode='w',encoding='utf-8',newline='') as file:
-        try:
-            writer = csv.writer(file)
-            #fields = None
-            writer.writerow(fields)
-            writer.writerows(data)
-        except:
-            return False
-        else:
-            return True
+def save_to_csv(students:list[dict],fileName:str)->None:
+    fileNameWithExtension:list = fileName + '.csv'
+    with open(fileNameWithExtension,mode = 'w',encoding = 'utf-8',newline = '') as file:
+        fieldnames:list[str] = ['姓名','國文','數學','英文','地理']
+        writer:DictWriter= csv.DictWriter(file,fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(students)
+    print("寫入成功")
