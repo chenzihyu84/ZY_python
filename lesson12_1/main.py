50e7cbdd1f9a13cd9b535f4fc104b0ba7b4b2e40
chenzihyu84 <150919672+chenzihyu84@users.noreply.github.com>
2023-12-23 01:20:52 +0000

    modify

---
+++ lesson12_1/main.py
@@ -0,0 +1,18 @@
+
+import pyinputplus as pyip
+from tools import getStudents,savetoCSV
+
+
+#students:list[list] = getStudents()
+#print(students)
+
+if __name__ == '__main__':
+
+    student_num:int = pyip.inputInt("請輸入學生人數(1~50):",min=1,max=50)
+    scores_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
+    students:list[list] = getStudents(student_num,scores_nums)
+    fileName = pyip.inputFilename("請輸入檔案名稱:")
+    if savetoCSV(fileName=fileName,data=students,subject_nums=scores_nums):
+        print("存檔成功")
+    else:
+        print("存檔失敗")
