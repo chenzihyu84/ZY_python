47ca45f0a338453ccc01050fdd7ddcd2ba37b338
chenzihyu84 <150919672+chenzihyu84@users.noreply.github.com>
2023-12-23 03:58:12 +0000

    modify

--- lesson12_1/main.py
+++ lesson12_1/main.py
@@ -6,5 +6,5 @@ from tools import getStudents,save_to_csv
 if __name__ == "__main__":
     student_num:int = pyip.inputInt("請輸入學生人數(1~50):",min=1,max=50)
     students:list[dict] = getStudents(student_num)
-    fileName = pyip.inputFilename("請輸入檔案名稱:")
+    fileName:str = pyip.inputFilename("請輸入檔案名稱:")
     save_to_csv(students,fileName)
